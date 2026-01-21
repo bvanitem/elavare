6a0d773f4ded6b1d6a4e6b06f0ddab7866d87436
bvanitem <blainevanitem2000@gmail.com>
2026-01-21 11:54:39 -0700

    added constants for colors ease of access

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color mainBackgroundColor = Color(0xECE8E4);
+const Color complimentBackgroundColor = Color(0xDFD3C6);
+const Color otherBrown = Color(0xB8A799);
+const Color fontColor = Color(0x565656);
+const Color buttonColor = Color(0x86B0CE);
+const Color accentColor = Color(0xFFFFFF);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:elavare/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
+        colorScheme:  .fromSeed(seedColor: fontColor),
         textTheme: GoogleFonts.montserratTextTheme(TextTheme())
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
